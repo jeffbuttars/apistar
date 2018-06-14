2c1c1b33ab90ea5fa657de65f32cc55a9090e60f
Jeff Buttars <jeffbuttars@gmail.com>
2018-06-14 08:11:21 -0600

    * Tunned up the status class to match the style of Django status names. * First, untested pass at a WebSocket class to manage each connection. * Minimized the Exception classes to leverage the HTTPException base     class in a better way

--- apistar/server/websocket.py
+++ apistar/server/websocket.py
@@ -7,71 +7,63 @@ from apistar.server.components import Component
 
 
 class Status():
-    ok = 1000
-    leaving = 1001
-    prot_error = 1002
-    unsupported_type = 1003
-    reserved_1004 = 1004
-    no_status = 1005
-    closed_abnormal = 1006
-    inalid_data = 1007
-    policy_violation = 1008
-    too_big = 1009
-    tls_fail = 1010
+    """
+    https://tools.ietf.org/html/rfc6455#page-45
+    """
 
     @property
-    def OK(self):
+    def WS_1000_OK(self):
         """
         1000 indicates a normal closure, meaning that the purpose for
         which the connection was established has been fulfilled.
         """
-        return self.ok
+        return 1000
 
     @property
-    def LEAVING(self):
+    def WS_1001_LEAVING(self):
         """
         1001 indicates that an endpoint is "going away", such as a server
         going down or a browser having navigated away from a page.
         """
-        return self.leaving
+        return 1001
 
     @property
-    def PROT_ERROR(self):
+    def WS_1002_PROT_ERROR(self):
         """
         1002 indicates that an endpoint is terminating the connection due
         to a protocol error.
         """
-        return self.prot_error
+        return 1002
 
     @property
-    def UNSUPPORTED_TYPE(self):
+    def WS_1003_UNSUPPORTED_TYPE(self):
         """
         1003 indicates that an endpoint is terminating the connection
         because it has received a type of data it cannot accept (e.g., an
         endpoint that understands only text data MAY send this if it
         receives a binary message).
         """
-        return self.unsupported_type
+        return 1003
 
     @property
-    def RESERVED_1004(self):
+    def WS_1004_RESERVED(self):
         """
         Reserved.  The specific meaning might be defined in the future.
         """
-        return self.reserved_1004
+        return 1004
 
     @property
-    def NO_STATUS(self):
+    def WS_1005_NO_STATUS(self):
         """
         1005 is a reserved value and MUST NOT be set as a status code in a
         Close control frame by an endpoint.  It is designated for use in
         applications expecting a status code to indicate that no status
         code was actually present.
         """
-        return self.no_status
+        return 1005
 
     @property
-    def CLOSED_ABNORMAL(self):
+    def WS_1006_CLOSED_ABNORMAL(self):
         """
         1006 is a reserved value and MUST NOT be set as a status code in a
         Close control frame by an endpoint.  It is designated for use in
@@ -79,20 +71,20 @@ class Status():
         connection was closed abnormally, e.g., without sending or
         receiving a Close control frame.
         """
-        return self.closed_abnormal
+        return 1006
 
     @property
-    def INALID_DATA(self):
+    def WS_1007_INALID_DATA(self):
         """
         1007 indicates that an endpoint is terminating the connection
         because it has received data within a message that was not
         consistent with the type of the message (e.g., non-UTF-8 [RFC3629]
         data within a text message).
         """
-        return self.inalid_data
+        return 1007
 
     @property
-    def POLICY_VIOLATION(self):
+    def WS_1008_POLICY_VIOLATION(self):
         """
         1008 indicates that an endpoint is terminating the connection
         because it has received a message that violates its policy.  This
@@ -100,26 +92,26 @@ class Status():
         other more suitable status code (e.g., 1003 or 1009) or if there
         is a need to hide specific details about the policy.
         """
-        return self.policy_violation
+        return 1008
 
     @property
-    def TOO_BIG(self):
+    def WS_1009_TOO_BIG(self):
         """
         1009 indicates that an endpoint is terminating the connection
         because it has received a message that is too big for it to
         process.
         """
-        return self.too_big
+        return 1009
 
     @property
-    def TLS_FAIL(self):
+    def WS_1010_TLS_FAIL(self):
         """
         1010 indicates that an endpoint (client) is terminating the
         connection because it has expected the server to negotiate one or
         more extension, but the server didn't return them in the response
         message of the WebSocket handshake.  The list of extensions that
         """
-        return self.tls_fail
+        return 1010
 
 
 status = Status()
@@ -132,30 +124,31 @@ class WSState(enum.Enum):
 
 
 class WebSocketNotConnected(HTTPException):
-    def __init__(self,
-                 detail: str = 'WebSocket is not connected or open',
-                 status_code: int = status.OK) -> None:
-        super().__init__(detail, 200)
+    default_status_code = status.WS_1000_OK
+    default_detail = 'WebSocket is not connected or open'
+
+
+class WebSocketDisconnect(HTTPException):
+    default_status_code = status.WS_1000_OK
+    default_detail = 'WebSocket has been disconnected'
 
 
 class WebSocketProtocolError(HTTPException):
-    def __init__(self,
-                 detail: str = 'WebSocket protocol error',
-                 status_code: int = status.PROT_ERROR) -> None:
-        super().__init__(detail, status_code)
+    default_status_code = status.WS_1002_PROT_ERROR
+    default_detail = 'WebSocket protocol error'
 
 
 class WebSocket(object):
+    """
+    https://github.com/django/asgiref/blob/master/specs/www.rst
+    """
     def __init__(self,
                  asgi_scope: ASGIScope,
                  asgi_send: ASGISend,
                  asgi_receive: ASGIReceive,
                  ) -> None:
 
-        if asgi_scope['type'] != 'websocket':
-            raise WebSocketProtocolError(
-                detail="ASGI scope is not 'websocket'"
-            )
+        assert asgi_scope['type'] == 'websocket'
 
         self._scope = asgi_scope
         self._asgi_send = asgi_send
@@ -163,11 +156,16 @@ class WebSocket(object):
         self._state = WSState.CLOSED
 
     @property
+    def subprotocols(self) -> list:
+        return self._scope.get('subprotocols', [])
+
+    @property
     def state(self):
         return self._state
 
-    async def send(self, data: typing.Union[str, bytes]):
-        if self.state == WSState.CLOSED:
+    async def send(self, data: typing.Union[str, bytes]) -> None:
+
+        if self.state != WSState.CONNECTED:
             raise WebSocketNotConnected()
 
         msg = {
@@ -180,33 +178,49 @@ class WebSocket(object):
             else:
                 msg['text'] = data
 
-        return await self._asgi_send(msg)
+        await self._asgi_send(msg)
 
-    async def receive(self):
+    async def receive(self) -> typing.Union[str, bytes]:
         if self.state != WSState.CONNECTED:
             raise WebSocketNotConnected()
 
         msg = await self._asgi_receive()
+
+        if msg['type'] == 'websocket.disconnect':
+            self.state = WSState.CLOSED
+            raise WebSocketDisconnect(status_code=msg['code'])
+
         return msg.get('text', msg.get('bytes'))
 
-    async def connect(self):
+    async def connect(self,
+                      subprotocol: str = None,
+                      close: bool = False,
+                      close_code: int = status.WS_1000_OK) -> None:
+
+        # Accept or Refuse an incoming connection
         if self.state != WSState.CLOSED:
             raise WebSocketProtocolError(
-                detail="Attempting to connect a WebSocket that is not closed"
+                detail="Attempting to connect a WebSocket that is not closed: %s" % self.state
             )
 
-        # Try to accept and upgrade the websocket
+        # Expecting a connect message
         msg = await self._asgi_receive()
 
         if msg['type'] != 'websocket.connect':
             raise WebSocketProtocolError(
-                'Expected websocket connection but got: %s' % msg['type'])
+                'Expected WebSocket `connection` but got: %s' % msg['type']
+            )
 
         self._state = WSState.CONNECTING
 
-        await self._asgi_send({'type': 'websocket.accept'})
+        if close:
+            await self.close(code=close_code)
+            return
 
-    async def accept(self, subprotocol: str = None):
+        # Try to accept and upgrade the websocket
+        await self.accept(subprotocol)
+
+    async def accept(self, subprotocol: str = None) -> None:
         if self.state != WSState.CONNECTING:
             raise WebSocketProtocolError(
                 detail="Attempting to accept a WebSocket that is not connecting"
@@ -219,12 +233,12 @@ class WebSocket(object):
         await self._asgi_send(msg)
         self._state = WSState.CONNECTED
 
-    async def close(self, code: int = status.OK):
-        if self.state != WSState.CONNECTED:
+    async def close(self, code: int = status.OK) -> None:
+        if self.state == WSState.CLOSED:
             raise WebSocketNotConnected()
 
         message = {
-            'type': 'websocket.disconnect',
+            'type': 'websocket.close',
             'code': code,
         }
 
